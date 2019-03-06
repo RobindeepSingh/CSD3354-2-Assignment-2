7f438106f9b9da5b24eafbf58584eda2d2a56e0e
visualstudio <visualstudio@122-PC21>
2019-03-06 11:48:07 -0500

    lab 9

--- Assignment 2/Program.cs
+++ Assignment 2/Program.cs
@@ -5,34 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-public class Program
+namespace DelegatesAndEvents
 {
-    static void Main(string[] args)
+    public delegate void MyDelegate();
+    public class DelegateExercises : MyDelegate
     {
-        DelegateExercises delegateExercises = new DelegateExercises();
-        delegateExercises.Method3();
-        Console.ReadLine();
+
     }
 }
-public delegate void MyDelegate();
-
-public class DelegateExercises
-{
-    void Method1()
-    
-         {
-            System.Console.WriteLine("Method1");
-
-        }
-        public void Method3()
-        {
-            MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate();
-        }
-    }
-
-
-
 
 
 
